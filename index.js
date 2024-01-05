7f179b9dcb4c6fb337f358ea089ce8c7babfc538
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-01-05 14:47:55 +0200

    add csv parse and functionality

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const { parse } = require('csv-parse')
 const fs = require ('fs')
 
+const results = [];
+
+//The createReadStream() method is an inbuilt application programming interface of the 
+// fs module which allow you
+// to open up a file/stream and read the data present in it.
+// Syntax: fs.createReadStream( path, options )
+// This gives us back a readable stream!
+//  As a readable stream, it emits various events during its lifecycle. 
+// Some of the key events emitted by a createReadStream are data, end, error, close, open.
+// These events provide hooks to react to different stages of the stream's lifecycle
+
+
 fs.createReadStream('kepler_data.csv')
+    .pipe(parse()) //pipe function connects a readable stream source to a writable stream destination.
     .on('data', (data) => {
-        resourceLimits.push(data);
+        results.push(data);
     })
-    .on('end', () =>);
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log(results);
+        console.log('done');
+    });
+
+    // parse();
